a33a149abb7f80cad41e5c8e1fe99edfba055955
Wesam Nasr Ahmed <wesamnasr999@gmail.com>
2025-07-16 10:58:18 +0300

    Add Some Errors

--- TestDevelopersManagement/Controllers/HomeController.cs
+++ TestDevelopersManagement/Controllers/HomeController.cs
@@ -22,8 +22,20 @@ namespace TestDevelopersManagement.Controllers
         {
             return View();
         }
-
+        int x=5;
+        int y=0
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
